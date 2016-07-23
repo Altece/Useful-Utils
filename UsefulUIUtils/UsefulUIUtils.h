7f7aba9a42b87ef1b4364e724486912bc6b55717
Steven Brunwasser <sbrunwasser@gmail.com>
2016-07-22 21:14:39 -0700

    Created some utilities to make AutoLayout much easier to deal with. UUILayoutAttribute abstracts away the pairing of a layout item and its attribute. UUILayoutScaffold abstracts away the need to use the NSLayoutRelation enum values.

---
+++ UsefulUIUtils/UsefulUIUtils.h
@@ -0,0 +1,21 @@
+//
+//  UsefulUIUtils.h
+//  UsefulUIUtils
+//
+//  Created by Steve Brunwasser on 7/22/16.
+//  Copyright © 2016 Steve Brunwasser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UsefulUIUtils.
+FOUNDATION_EXPORT double UsefulUIUtilsVersionNumber;
+
+//! Project version string for UsefulUIUtils.
+FOUNDATION_EXPORT const unsigned char UsefulUIUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UsefulUIUtils/PublicHeader.h>
+
+#import <UsefulUIUtils/UUILayoutAttribute.h>
+#import <UsefulUIUtils/UUILayoutScaffold.h>
+
